c9714de2f4cb7c06c8dc237e15f11476352d01fe
Suyash Gupte <gupte.suyash11@gmail.com>
2023-01-19 18:37:57 +0530

    Update change-to-dark-mode.py

--- change-to-dark-mode.py
+++ change-to-dark-mode.py
@@ -2,5 +2,5 @@ data={}
 
 data["workbench.colorTheme"] = "Default Dark+"
 
-with open("~/.local/share/code-server/User/settings.json", "w") as jsonFile:
+with open("/root/.local/share/code-server/User/settings.json", "w") as jsonFile:
     json.dump(data, jsonFile)
